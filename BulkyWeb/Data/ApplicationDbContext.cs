38af215bf05c7f275cff16ec712b01acb9ca4b35
Janith Udayanga <janith0000@gmail.com>
2023-07-13 18:08:17 +0530

    Established database connection & created initial db records

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext
+            (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fish", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Elephants", DisplayOrder = 2},
+                new Category { Id =3, Name = "Rabbits", DisplayOrder = 3}
+                );
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] //not required here since by default the item with modelnameID(CategoryID) or Id will be taken as the primary key.
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
